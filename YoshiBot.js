516c86be2a16fae5c4db2ffa1386dc551f336347
MarioGraceZ22 <ianmisael_99@hotmail.com>
2016-07-11 21:22:22 -0600

    I did it, mom. :eyes:
    Sorry about that long stream of commits. I needed to test the functionality of !updt, but that's all over with now. Bot will now properly make a git pull to update itself every time I push a change and call the command. So, this means I can update him remotely. Yay me!

--- YoshiBot.js
+++ YoshiBot.js
@@ -153,7 +153,7 @@ bot.on("message", function (msg) {
                     break;
 
                 case "!ping": //Checks if bot is alive.
-                    bot.sendMessage(msg.channel, "Poooooooooong!");
+                    bot.sendMessage(msg.channel, "Pong!");
                     break;
 
                 case "!help": //Displays help message.
