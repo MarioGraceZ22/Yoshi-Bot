88eef84429edf42847b84888c21cb3f8a3c269e8
MarioGraceZ22 <ianmisael_99@hotmail.com>
2016-08-16 15:14:27 -0700

    More comments to debug !info

--- YoshiBot.js
+++ YoshiBot.js
@@ -460,9 +460,8 @@ bot.on("message", function (msg) {
 
                                         var userServerDetails = msg.server.detailsOfUser(user);
                                         infoString += "- This user has the role(s) **" + userServerDetails.roles + "** in this server.\n- **" + user.name + "'s** nickname is **" + userServerDetails.nick + "** in this server.\n- **" + user.name + "#" + user.discriminator + "** joined this server in **";
-                                        //timeJoined = helpers.unix_to_time(userServerDetails.joinedAt);
-                                        //timeJoined = "nicemem";
-                                        infoString += userServerDetails.joinedAt + "**.\n\n- The ID of server **" + msg.server.name + "** is **" + msg.server.id + "**.\n- There are **" + msg.server.users.length + "** users in this server.";
+                                        timeJoined = helpers.unix_to_time(userServerDetails.joinedAt);
+                                        infoString += timeJoined + "**."//\n\n- The ID of server **" + msg.server.name + "** is **" + msg.server.id + "**.\n- There are **" + msg.server.users.length + "** users in this server.";
                                         bot.sendMessage(msg.channel, infoString);
                                     }
                                 });
